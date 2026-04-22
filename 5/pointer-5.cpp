1fc0f9b6daf64a88d808a3abbf6923c56bdbacc9
Jongyeon Won <samuelwon@@kentech.ac.kr>
2026-04-22 17:22:08 +0000

    04.23

--- 5/pointer-5.cpp
+++ 5/pointer-5.cpp
@@ -8,6 +8,14 @@ int main(int argc, char *argv[])
 	char str2[6] = {'w', 'o', 'r', 'l', 'd', 0};
 	char *p;
 	int i; 
+
+	p = str2;
+
+	cout << "str1 = " << str1 << endl;
+	cout << "str2 = " << str2 << endl;
+
+	for (i=-10; i<10; i++)
+	cout << "p["<<i<<"] = " << p[i] << endl;
 	return 0;
 }
 	// 이것도 복습
